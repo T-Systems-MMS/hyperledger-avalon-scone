843ecbbe85d8864d67629f100c221d4b26e3ce13
mujtabaidrees94 <mujtabaidrees@gmail.com>
2021-02-08 02:23:08 -0800

    updated license info

--- avalon-scone/worker_manager/avalon_enclave_manager/scone/scone_enclave_info.py
+++ avalon-scone/worker_manager/avalon_enclave_manager/scone/scone_enclave_info.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Intel Corporation
+# Copyright 2020 Mujtaba Idrees
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- examples/scone_apps/generic_client_scone.py
+++ examples/scone_apps/generic_client_scone.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 # Copyright 2019 Intel Corporation
+# Copyright 2019 Mujtaba Idrees
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
